93b5c550a9d2d9b9160876e99e3bbf79e382ce5a
prernaty <prerna.tyagi96@gmail.com>
2021-05-06 12:43:12 +0530

    Adding ejs files for home controller

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const port = 8000;
 
 
 // use express router
-app.use('/',require('./routes/index'))
+app.use('/',require('./routes/index'));
+
+
+//setting view engine
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
 
 app.listen(port,function(err){
     if(err)
